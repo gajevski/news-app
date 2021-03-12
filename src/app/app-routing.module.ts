705db12949c5e8ddeb51d7fa1599efa7e38f1746
gajevski <mikolajxgajewski@gmail.com>
2021-03-12 17:50:09 +0100

    Add share module
    Add shared module and lazy load it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
